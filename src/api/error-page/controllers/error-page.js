6a2555e28baa4b14d18badee15a6e0cb2f8c1afd
unknown <contact@christophe-le-goff.com>
2022-09-20 22:54:23 +0200

    delete all

--- src/api/error-page/controllers/error-page.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- *  error-page controller
- */
-
-const { createCoreController } = require('@strapi/strapi').factories;
-
-module.exports = createCoreController('api::error-page.error-page');
--- src/api/form-devi/services/form-devi.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * form-devi service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::form-devi.form-devi');
