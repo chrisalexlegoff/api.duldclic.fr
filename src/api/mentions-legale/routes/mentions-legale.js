4198830d98e641f4c53a30aaf42ea02fffe2051b
unknown <contact@christophe-le-goff.com>
2022-10-09 11:26:08 +0200

    add Cookies Manager

---
+++ src/api/mentions-legale/routes/mentions-legale.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mentions-legale router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mentions-legale.mentions-legale');
---
+++ src/api/mentions-legale/services/mentions-legale.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mentions-legale service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::mentions-legale.mentions-legale');
