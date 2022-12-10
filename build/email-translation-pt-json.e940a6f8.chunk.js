0037fa35292c97aa0804e11666513fd0cc7824cc
unknown <contact@christophe-le-goff.com>
2022-12-10 18:23:45 +0100

    add users content for test dashboard in front

--- build/email-translation-pt-json.e940a6f8.chunk.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunkapi_dualdclic_fr=self.webpackChunkapi_dualdclic_fr||[]).push([[3166],{18210:c=>{c.exports={}}}]);
--- config/plugins.js
+++ config/plugins.js
@@ -1,5 +1,26 @@
 module.exports = ({ env }) => {
   return {
+    email: {
+      config: {
+        provider: "nodemailer",
+        providerOptions: {
+          host: env("SMTP_HOST", "smtp.example.com"),
+          port: env("SMTP_PORT", 587),
+          auth: {
+            user: env("SMTP_USERNAME"),
+            pass: env("SMTP_PASSWORD"),
+          },
+          tls: {
+            rejectUnauthorized: false,
+          },
+          // ... any custom nodemailer options
+        },
+        settings: {
+          defaultFrom: "contact@dualdclic.fr",
+          defaultReplyTo: "contact@dualdclic.fr",
+        },
+      },
+    },
     "upload-plugin-cache": {
       enabled: true,
       config: {
